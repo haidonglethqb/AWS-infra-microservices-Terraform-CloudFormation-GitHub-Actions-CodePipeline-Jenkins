f35d04e72954a64301dbd611bf2d7ea70dca3efb
HAI <22520378@gm.uit.edu.vn>
2025-06-07 22:34:38 +0700

    Format terraform code

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -2,10 +2,10 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.31.2"
 
-  cluster_name                  = "chatbot-cluster"
-  cluster_version               = "1.28"
-  vpc_id                        = module.vpc.vpc_id
-  enable_irsa                   = true
+  cluster_name                   = "chatbot-cluster"
+  cluster_version                = "1.28"
+  vpc_id                         = module.vpc.vpc_id
+  enable_irsa                    = true
   cluster_endpoint_public_access = true
 
   subnet_ids = module.vpc.private_subnets
