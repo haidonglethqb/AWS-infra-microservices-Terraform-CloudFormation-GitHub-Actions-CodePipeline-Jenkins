21589db34865b136acc1347a6a0cb644ce46596e
HAI <22520378@gm.uit.edu.vn>
2025-06-07 22:17:25 +0700

    new fix

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_name" {
-  description = "Name of the EKS cluster"
-  value       = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  description = "Endpoint URL of the EKS cluster"
-  value       = module.eks.cluster_endpoint
-}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
-}
